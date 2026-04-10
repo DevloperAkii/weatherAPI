284ec2a46b8c9f7b49b4611329db4d946fc9ddcd
Akshit Patel <devloperAkii0709@gmail.com>
2026-04-10 12:03:40 +0530

    added the humidity and more info

--- back-end/server.js
+++ back-end/server.js
@@ -14,6 +14,18 @@ app.use("/v1/weather", router);
 //     res.send(404).send({error : "not found"});
 // })
 
+//AI Copied Code
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://mellifluous-pavlova-a55246.netlify.app');
+  res.header('Access-Control-Allow-Private-Network', 'true');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204); // Important for preflight requests
+  }
+  next();
+});
+//
+
 app.listen(8000, () => {
     console.log("listening to port 8000");
 });
--- front-end/index.js
+++ front-end/index.js
@@ -1,7 +1,57 @@
 const celcusText = document.getElementById("celcius-text");
+const footerDisplay = document.getElementById("future-display");
+
+const precip = document.getElementById("precipitation-value");
+const humidity = document.getElementById("humidity-value");
+const wind = document.getElementById("wind-value");
+
+const day_time = document.getElementById("day-time");
+const condition = document.getElementById("condition");
+
+const dateNow = new Date();
+
+const day = dateNow.toLocaleDateString('en-US', { weekday: 'long' })
+const hours = dateNow.getHours();
+
+const ampm = hours >= 12 ? 'PM' : 'AM';
 
 const API = "http://localhost:8000/v1/weather"
-
 fetch(API).then(res => res.json()).then(res => {
-    celcusText.innerHTML = `${res[0].temp}<sup>C</sup><sup>F</sup>`;
+    const tempFre = parseFloat(res[0].tempmax);
+    const tempCelcius = (tempFre - 32) * 5/9;
+    celcusText.innerHTML = `${Math.round(tempCelcius)}<sup class="sup-celc">C</sup><sup>F</sup>`;
+    precip.innerText = `${res[0].precip}%`;
+    humidity.innerText = `${res[0].humidity}%`;
+    wind.innerText = `${res[0].windspeed} km/h`;
+
+    day_time.innerText = `${day}, ${hours}:00 ${ampm}`;
+    condition.innerText = `${res[0].conditions}`;
+
+    console.log(res.length)
+
+    for(let i = 0; i < 10; i++){
+        const day = dateNow.toLocaleDateString('en-US', { weekday: 'long' })
+
+        const card = document.createElement("div");
+        card.setAttribute("class", "card");
+
+        const headText = document.createElement("h4");
+        headText.innerText = `${day}`;
+
+        const icon = document.createElement("img");
+        icon.setAttribute("src", "img/sun-yellow-spiral.png");
+        const tempP = document.createElement("p");
+
+        const tempFre = parseFloat(res[i].tempmax);
+        const tempCelcius = (tempFre - 32) * 5/9;
+
+        tempP.innerHTML = `${Math.round(tempCelcius)}<sup>◦</sup>`;
+
+        card.appendChild(headText);
+        card.appendChild(icon);
+        card.appendChild(tempP);
+
+        footerDisplay.appendChild(card);
+        dateNow.setDate(dateNow.getDate() + 1);
+    }
 });
